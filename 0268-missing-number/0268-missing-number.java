2e20527b96234df678a5f339053259adf3bcce08
CHEDADEEPU DHANUSH <chedadeepudhanush15@gmail.com>
2026-07-21 19:58:02 +0530

    Time: 0 ms (100%), Space: 47.3 MB (49.08%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int exprassion =  n * (n + 1) / 2;
+        for(int i = 0; i < nums.length; i++){
+            exprassion -= nums[i];
+
+        }
+        return exprassion;
+    }
+}
